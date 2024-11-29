1a2328f3b6f6133cc84671aa4f0ad468da64d257
Maksim Rastoskuev <maksimrastoskuev@ya.ru>
2024-11-29 23:42:19 +0700

    add brain-gcd game and update files

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+/* eslint-disable import/extensions */
+import startGameGCD from '../src/games/game-gcd.js';
+
+startGameGCD();
---
+++ src/games/game-gcd.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/extensions */
+import gameLogic from '../index.js';
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max + 1 - min));
+const description = 'Find the greatest common divisor of given numbers.';
+
+const getGameGCD = () => {
+  const randomNumberOne = getRandomNumber(1, 100);
+  const randomNumberTwo = getRandomNumber(1, 100);
+  const lowestNumber = randomNumberOne > randomNumberTwo ? randomNumberTwo : randomNumberOne;
+  let maxDivisor = '1';
+
+  for (let i = 2; i <= lowestNumber; i += 1) {
+    if (randomNumberOne % i === 0 && randomNumberTwo % i === 0) {
+      maxDivisor = String.i;
+    }
+  }
+
+  const question = `${randomNumberOne} ${randomNumberTwo}`;
+  return [question, maxDivisor];
+};
+
+const startGameGCD = () => gameLogic(description, getGameGCD);
+
+export default startGameGCD;
