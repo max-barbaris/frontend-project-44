9a3693d593554f9ad954814c1646ed1d3b9bec43
Maksim Rastoskuev <maksimrastoskuev@ya.ru>
2024-12-01 18:58:32 +0700

    syntax correction

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import readlineSync from 'readline-sync';
 
 const gameLogic = (description, gameData) => {
-  console.log('Welcone to the Brain Games!');
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(description);
