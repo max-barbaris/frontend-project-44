20db0282799f5e33748608ae325b7c5f23ca3da5
Maksim Rastoskuev <maksimrastoskuev@ya.ru>
2024-12-01 20:48:27 +0700

    fix problem in game-prime.js

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -4,7 +4,7 @@ import getRandomNumber from '../random.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (number) => {
-  for (let i = 2; i < number / 2; i += 1) {
+  for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
       return false;
     }
